84f8ebf6494926d2a275580a6b9a3c8fb9e956c4
Pranayrebeyro <pranayrebeyro.com>
2026-06-18 23:28:40 +0530

    Updated requirements

--- utilis/gemini_response.py
+++ utilis/gemini_response.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
+import streamlit as st
+import google.generativeai as genai
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
