93d059190616a3b3fd8a01448ce720fbe21567e6
Pranayrebeyro <pranayrebeyro.com>
2026-06-18 23:38:06 +0530

    Updated requirements

--- utilis/gemini_response.py
+++ utilis/gemini_response.py
@@ -6,7 +6,7 @@ import google.generativeai as genai
 
 load_dotenv()
 
-genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
